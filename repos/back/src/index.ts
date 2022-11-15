dd1701bf3a873ba84d7ea922fab870bf7fed19cf
Eron Alves <eron.a.silva@capgemini.com>
2022-11-15 02:21:35 -0300

    Correct some bugs

--- repos/back/src/index.ts
+++ repos/back/src/index.ts
@@ -2,6 +2,7 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 import { cadastrar, RegisterData } from "./cadastro";
 import { login } from "./login";
+import { sha256 } from "js-sha256";
 
 const app = express();
 
@@ -20,8 +21,23 @@ app.post("/cadastro", (req, res) => {
 });
 
 app.get("/login", (req, res) => {
+  const authorization = req.headers.authorization;
+  if (!authorization?.startsWith("Basic")) {
+    res.status(400);
+    res.send({
+      errorMessage:
+        "Please check the validation method for this authentication",
+    });
+  }
+
+  const credentials = atob(<string>authorization?.split(" ")[1]).split(":");
+  const auth: RegisterData = {
+    username: credentials![0],
+    password: sha256(credentials![1]),
+  };
+
   try {
-    const jwtPayload = login(req.body);
+    const jwtPayload = login(auth);
     res.cookie("jwt-auth", jwtPayload, {
       maxAge: 24 * 60 * 60 * 1000,
       httpOnly: true,
