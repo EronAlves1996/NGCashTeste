b4eb72c17b9dbdcf58ec3fbd6231e7a1a92d814d
Eron Alves <eron.a.silva@capgemini.com>
2022-11-16 18:04:29 -0300

    Maded cadastro page

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -9,11 +9,13 @@ import {
 import "./index.css";
 import { Index } from "./Index";
 import { App } from "./App";
+import { Cadastro } from "./Cadastro";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index element={<Index />} />
+      <Route path="/cadastro" element={<Cadastro />} />
     </Route>
   )
 );
